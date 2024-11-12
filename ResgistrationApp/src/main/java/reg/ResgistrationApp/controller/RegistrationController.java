189ab89624d54d6b10d025ab783f6788c5011532
quest <quest@DESKTOP-R3CNG5N>
2024-11-12 12:01:54 +0530

    third commit

--- ResgistrationApp/src/main/java/reg/ResgistrationApp/controller/RegistrationController.java
+++ ResgistrationApp/src/main/java/reg/ResgistrationApp/controller/RegistrationController.java
@@ -18,7 +18,7 @@ import reg.ResgistrationApp.model.User;
 import reg.ResgistrationApp.service.UserService;
 import reg.ResgistrationApp.util.LoginRequest;
 import reg.ResgistrationApp.util.UserDto;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://shamnasproject.netlify.app")
 @RestController
 public class RegistrationController {
    @Autowired
